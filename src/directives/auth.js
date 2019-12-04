439fa8a07e4604a5f28f945af5164d8978cc98cb
daniel fang <54574438+Daniel-Fang@users.noreply.github.com>
2019-12-04 16:44:54 +0800

    add vue plugin method

--- src/directives/auth.js
+++ src/directives/auth.js
@@ -11,3 +11,34 @@ function install(Vue, options = {}) {
 }
 
 export default { install };
+
+/**
+ *
+ * @param {Vue 实例} Vue
+ * @param {*} options
+ */
+const install = function (Vue, options = {}) {
+    // 1.添加全局方法或属性 手动调用
+    Vue.myGlobalMethod = function (methodOptions) {
+        alert(methodOptions);
+    };
+
+    // 2.添加实例方法 手动调用
+    Vue.prototype.$msg = function (methodOptions) {
+        alert(methodOptions.message);
+    };
+
+    // 3. 添加全局资源 手动调用
+    Vue.directive('my-directive', {
+        bind (el, binding, vnode, oldVnode) {
+            // 逻辑...
+        }
+    });
+
+    // 4. 注入组件选项 主动混入，在指定生命周期内自动调用
+    Vue.mixin({
+        created: function () {
+            // 逻辑...
+        }
+    });
+};
