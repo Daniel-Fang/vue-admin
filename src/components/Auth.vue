7ce467043c3ec57f75a631c68bf741a1a5ac957f
daniel fang <54574438+Daniel-Fang@users.noreply.github.com>
2019-12-15 20:44:23 +0800

    Update Auth.vue

--- src/components/Auth.vue
+++ src/components/Auth.vue
@@ -6,7 +6,6 @@ export default {
     props: {
         authority: {
             type: Array,
-            required: true,
             default: function() {
                 return ['user']
             }
